d29ad50c1c1b64b77dad1b6c804fbd7a3dca353e
Lars Poetzel <lars.poetzel@gmail.com>
2022-11-02 16:08:57 +0100

    hope it works now lol

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Categories from './pages/Categories';
 import Home from "./pages/Home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Categories />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/categories" element={<Categories/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
     <div>
+      <Link to="categories"> go to categories page</Link>
         <ul>
             <li>
                 <b>file name:</b> Home.jsx
