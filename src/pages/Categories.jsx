ab9bf498c23b8d4defa4baa30ea32415b24daa22
Lars Poetzel <lars.poetzel@gmail.com>
2022-11-02 15:37:30 +0100

    initial setup

---
+++ src/pages/Categories.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div>
+        <ul>
+            <li>
+                <b>file name:</b> Categories.jsx
+            </li>
+            <li>
+                <b>component name:</b> Categories
+            </li>
+        </ul>
+
+
+    </div>
+  )
+}
+
+export default Categories
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <ul>
+            <li>
+                <b>file name:</b> Home.jsx
+            </li>
+            <li>
+                <b>component name:</b> Home
+            </li>
+        </ul>
+
+
+    </div>
+  )
+}
+
+export default Home
